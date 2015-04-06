a12b475d892f94a0c1fd3066d7c4437a84980004
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2015-04-06 12:13:21 +0200

    move reusable function to rosalib.py

--- python_village/a002_variables_and_some_arithmetic.py
+++ python_village/a002_variables_and_some_arithmetic.py
@@ -6,20 +6,14 @@
 
 #Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
 
+import rosalib
 
 def hyopotenuse(a, b):
     return (a * a) + (b * b)
 
-def is_integer(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-
 def main(limit):
     a, b = raw_input().split()
-    if (not is_integer(a)) or (not is_integer(b)):
+    if (not rosalib.is_integer(a)) or (not rosalib.is_integer(b)):
         return
     a = int(a)
     b = int(b)
