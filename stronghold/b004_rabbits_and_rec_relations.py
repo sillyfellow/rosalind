bd53672cebe86cce41411c2eab0e915299982866
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2015-04-06 15:16:05 +0200

    use memoization for bunny mating

--- stronghold/b004_rabbits_and_rec_relations.py
+++ stronghold/b004_rabbits_and_rec_relations.py
@@ -5,13 +5,24 @@
 
 import rosalib
 
+def fibo_with_larger_litter_memoized(n, k):
+    fibs = {1: 1, 2: 1}
+
+    def memoized_fib(m):
+        if m not in fibs:
+            fibs[m] = memoized_fib(m - 1) + k * memoized_fib(m - 2)
+        return fibs[m]
+
+    return memoized_fib(n)
+
+
 def fibo_with_larger_litter_iter(n, k):
     a, b = 1, 1
-    while n > 1:
+    while n > 2:
         a = b + k * a
         b = a + k * b
         n = n - 2
-    return b if n == 0 else a
+    return a if n == 1 else b
 
 def fibo_with_larger_litter_rec(n, k):
     if n <= 2:
@@ -21,6 +32,7 @@ def fibo_with_larger_litter_rec(n, k):
 def main():
     a, b = rosalib.get_input_integers(2)
     print fibo_with_larger_litter_iter(a, b)
+    print fibo_with_larger_litter_memoized(a, b)
     print fibo_with_larger_litter_rec(a, b)
 
 if __name__ == '__main__':
